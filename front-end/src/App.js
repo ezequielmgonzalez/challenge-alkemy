279ac71a2880106cc4b562ca174f762fe02dbce8
Ezequiel Gonzalez <zeque.mdq.95@gmail.com>
2022-04-29 15:08:47 -0300

    Initiating front-end for the app

---
+++ front-end/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+// import Home from "./pages/Home";
+// import Movements from "./pages/Movements";
+// import LittleNav from "./components/LittleNav";
+
+function App() {
+  const App = () => (
+    <div>
+      {/* <LittleNav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movements" element={<Movements />} />
+      </Routes> */}
+      CHALLENGE
+    </div>
+  );
+  return <App />;
+}
+
+export default App;
