e7caa1066d4f53c74da9900b2db8d114dbc94a9e
Ezequiel Gonzalez <zeque.mdq.95@gmail.com>
2022-04-29 16:02:38 -0300

    New component (shows current balance) and added to Home

---
+++ front-end/src/components/CurrentBalance.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+// Displays the current balance resulting from all transactions
+function CurrentBalance() {
+  const [balance, setBalance] = React.useState(0);
+  // Get the account by doing a fetch to the back-end
+  const getBalance = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/movements/balance");
+      const jsonData = await response.json();
+      setBalance(jsonData);
+    } catch (e) {
+      console.error(e.message);
+    }
+  };
+
+  // Makes a fetch request tou our REST api everytime this is component is rendered
+  React.useEffect(() => {
+    getBalance();
+  }, []);
+
+  return (
+    <div>
+      <h2 className="smallText mb-0">Current Balance</h2>
+      <h2 className={`mt-0 balance ${balance <= 0 ? "negative" : "positive"}`}>
+        {balance <= 0 ? `-$${Math.abs(balance)}` : `$${balance}`}
+      </h2>
+    </div>
+  );
+}
+
+export default CurrentBalance;
--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,17 +1,16 @@
 import React from "react";
-// import { Link } from "react-router-dom";
 // import PreviewMovements from "../components/PreviewMovements";
-// import CurrentBalance from "../components/CurrentBalance";
-import { Button, Container } from "react-bootstrap";
+import CurrentBalance from "../components/CurrentBalance";
+import { Container } from "react-bootstrap";
 
 function Home() {
   return (
     <Container>
-      {/* <div className="App">
+      <div className="App">
         <h1>Home</h1>
         <CurrentBalance />
-        <PreviewMovements />
-      </div> */}
+        {/* <PreviewMovements /> */}
+      </div>
     </Container>
   );
 }
