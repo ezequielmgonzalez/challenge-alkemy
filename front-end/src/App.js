54372064e5c245d2d9531eb1fb719f6e734d3fe9
Ezequiel Gonzalez <zeque.mdq.95@gmail.com>
2022-04-29 15:43:10 -0300

    Pages added: Home and Movements

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,19 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-// import Home from "./pages/Home";
-// import Movements from "./pages/Movements";
+import Home from "./pages/Home";
+import Movements from "./pages/Movements";
 // import LittleNav from "./components/LittleNav";
 
 function App() {
   const App = () => (
     <div>
-      {/* <LittleNav />
+      {/* <LittleNav /> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movements" element={<Movements />} />
-      </Routes> */}
-      CHALLENGE
+      </Routes>
     </div>
   );
   return <App />;
--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import PreviewMovements from "../components/PreviewMovements";
-import CurrentBalance from "../components/CurrentBalance";
+// import { Link } from "react-router-dom";
+// import PreviewMovements from "../components/PreviewMovements";
+// import CurrentBalance from "../components/CurrentBalance";
 import { Button, Container } from "react-bootstrap";
 
 function Home() {
   return (
     <Container>
-      <div className="App">
+      {/* <div className="App">
         <h1>Home</h1>
         <CurrentBalance />
         <PreviewMovements />
-      </div>
+      </div> */}
     </Container>
   );
 }
