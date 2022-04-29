57132681209813034039c94a3b782b342798764f
Ezequiel Gonzalez <zeque.mdq.95@gmail.com>
2022-04-29 15:54:38 -0300

    New component that allows the addition of movements

---
+++ front-end/src/components/InputMovement.js
@@ -0,0 +1,37 @@
+import React from "react";
+import ModalMovement from "./ModalMovement";
+
+// Allows you to create new transactions that will be added to the list and will impact the current balance.
+const InputMovement = () => {
+  const onSubmitForm = async (e, movement) => {
+    // Handle the form submitted
+
+    // Prevent from refresh
+    e.preventDefault();
+    try {
+      // const body = { amount, concept, typeM, dateM };
+      await fetch("http://localhost:5000/movements", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(movement),
+      });
+      //  Once the response has been sent, itś going to refresh and show the changes
+      window.location = "/";
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <ModalMovement
+        btnLabel="+"
+        title="Input movement"
+        onSubmitForm={onSubmitForm}
+        disableMovementType={false}
+      />
+    </React.Fragment>
+  );
+};
+
+export default InputMovement;
--- front-end/src/pages/Movements.js
+++ front-end/src/pages/Movements.js
@@ -1,16 +1,16 @@
 import React from "react";
-// import InputMovement from "../components/InputMovement";
+import InputMovement from "../components/InputMovement";
 // import ListMovements from "../components/ListMovements";
 import { Container } from "react-bootstrap";
 
 function Movements() {
   return (
     <Container>
-      {/* <div className="App">
+      <div className="App">
         <h1>Movements</h1>
         <InputMovement />
-        <ListMovements />
-      </div> */}
+        {/* <ListMovements /> */}
+      </div>
     </Container>
   );
 }
